6952d341961cf62d36547b81089d4e377b155dff
Landon Reasoner <reasoner.landon90@gmail.com>
2021-04-17 18:03:58 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_catchphrase_to_character.rb
+++ db/migrate/006_add_catchphrase_to_character.rb
@@ -1,4 +1,4 @@
-class AddCatchphraseToCharacter < Active::Migration[5.2]
+class AddCatchphraseToCharacter < ActiveRecord::Migration[5.2]
   
   def change 
     add_column :characters, :catchphrase, :string 
