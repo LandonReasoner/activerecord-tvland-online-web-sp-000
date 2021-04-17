c9b5430dfbfb48eb7a07d12f7257f327a1f65b07
Landon Reasoner <reasoner.landon90@gmail.com>
2021-04-17 18:13:53 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
   
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+  
+  def actor_list
+    self.characters.collect do |character|
+      character.actor.full_name
+    end
+  end 
+  
 end
