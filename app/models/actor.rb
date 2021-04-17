b22cd8f4176876f45401f0a2d7d594851fd6b781
Landon Reasoner <reasoner.landon90@gmail.com>
2021-04-17 17:39:08 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,7 +3,13 @@ class Actor < ActiveRecord::Base
   has_many :characters
 
   def full_name 
-    '#{self.firs_name} #{self.last_name}'
+    "#{self.firs_name} #{self.last_name}"
+  end 
+  
+  def list_roles
+    self.characters.collect do |character|
+      "#{character.name} - #{character.show.name}"
+    end.join
   end 
 
 end
